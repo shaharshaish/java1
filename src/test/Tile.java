219262ff31b900aa36c9ae9ef9ed44f699e5a55b
Shahar Shaish <sshaish2011@gmail.com>
2024-04-14 13:28:12 +0300

    first

---
+++ src/test/Tile.java
@@ -0,0 +1,33 @@
+package test;
+
+
+import java.util.Objects;
+
+public class Tile {
+public final char letter;
+public final int score;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tile tile = (Tile) o;
+        return letter == tile.letter && score == tile.score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(letter, score);
+    }
+
+    private Tile(char letter, int score) {
+        this.letter = letter;
+        this.score = score;
+    }
+
+public static class Bag{
+        int[] numOfLetters;
+
+
+}
+}
